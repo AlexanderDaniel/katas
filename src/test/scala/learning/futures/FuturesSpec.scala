bb29bd8eb95b78c9b6884b3af07d70c15702b64a
Alexander Daniel <alexander.daniel@gmx.at>
2013-08-13 14:09:31 +0200

    Executing futures sequentially

--- src/test/scala/learning/futures/FuturesSpec.scala
+++ src/test/scala/learning/futures/FuturesSpec.scala
@@ -10,23 +10,44 @@ class FuturesSpec extends Specification {
 
   "executing futures sequentially" should {
     // See also http://stackoverflow.com/questions/13836725/sequentially-combine-arbitrary-number-of-futures-in-scala
-    "run them one after the other" in {
+    "run them one after the other (task returns Unit)" in {
       def task(n: Int): Future[Unit] = Future {
         println(s"$n is sleeping...")
         TimeUnit.SECONDS.sleep(1)
         println(s"$n is awake")
         ()
       }
-      val future: Future[Unit] = List(1, 2, 3).foldLeft(Future(())) {
-        (z, x) => z flatMap {
+      val combinedFuture: Future[Unit] = List(1, 2, 3).foldLeft(Future(())) {
+        (future, x) => future flatMap {
           _ => task(x)
         }
       }
-      Await.result(future, Duration.Inf) === ()
+      Await.result(combinedFuture, Duration.Inf) === ()
     }
+
+//    "run them one after the other but I want the results" in {
+//      def task(n: Int): Future[Int] = Future {
+//        println(s"I want the results: $n is sleeping...")
+//        TimeUnit.SECONDS.sleep(1)
+//        println(s"$n is awake")
+//        n
+//      }
+//      val futures: List[Future[Int]] = List(1,2,3).scanLeft(Future(0)) {
+//        (z, x) => z map { result =>
+//          task(x)
+//          result
+//        }
+//      }
+//      val future: Future[List[Int]] = Future.sequence(futures)
+//
+//    }
   }
 
+
+
   "the futures start running as soon as they are created" should {
+    // See also comment http://stackoverflow.com/a/11158557/1388926 of Viktor Klang in
+    // http://stackoverflow.com/questions/11157552/how-to-combined-futures-of-different-types-into-one-new-future-without-using-zip
     "demonstrate that" in {
       def task(n: Int): Future[Int] = Future {
         println(s"the futures start running as soon as they are created: $n is sleeping")
